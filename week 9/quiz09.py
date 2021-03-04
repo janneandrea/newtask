a6144c9cd2fe9be3b322ff1b2561d484182bb300
janneandrea <janneandrea@byui.edu>
2021-03-04 16:50:37 -0500

    intent commit

--- week 9/quiz09.py
+++ week 9/quiz09.py
@@ -1,5 +1,11 @@
-x = 5
+my_list =(1,-2,2,3,4,5,8,1000)
 
-x + 1
+smallest = 0
 
-print(x)
+for value in my_list:
+
+    if value <= smallest:
+
+        smallest = value
+
+print(f"The smallest is {smallest}")
