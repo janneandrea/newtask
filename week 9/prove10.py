fa44616536d8e2d7b1fa123580b3164a3fd64d22
janneandrea <janneandrea@byui.edu>
2021-03-10 17:37:45 -0500

    intent commit

---
+++ week 9/prove10.py
@@ -0,0 +1,43 @@
+ #welcome to the user
+print("Welcome to the Shopping cart Program!")
+
+#Set lists
+cart =[]
+prices = []
+
+#Menu
+Print("Please choose one of the following options:"/n)
+print("1.Add a new item")
+print("2.Display the contents of the shopping cart")
+print("3.Remove an item")
+print("4.Compute the total")
+print("5.Quit")
+
+action =("Please enter an action: ")
+
+# Add several items
+if action =="1"
+   add_items = input("What item would you like to add to the shopping cart? ")
+   items.append(add_items)
+   print(add_items)
+
+# Display items
+if action =="2"
+   display_items = input("This is what you have in your shopping cart: ")
+
+
+
+#Remove items
+if action =="3"
+   display_items = input("What item would you like to remove from the shopping cart? ")
+   items.pop(display_items)
+   print(display_items)
+
+# Show total
+if action =="4"
+
+
+
+#quit
+if action =="5"
+print("Thank you. Goodbye.")
