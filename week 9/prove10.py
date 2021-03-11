4944531f89bc7022d622170ad02203c2024f6d88
janneandrea <janneandrea@byui.edu>
2021-03-11 16:55:53 -0500

    intent commit

--- week 9/prove10.py
+++ week 9/prove10.py
@@ -18,13 +18,13 @@ while action !="5":
 action = input("Please enter an action: 1,2,3,4,5 "/n)
 
 # Add several items
-    if action =="1"
+    if action =="1":
        add_items = input("What item would you like to add? ")
        items.append(add_items)
        print(f"{add_items}has been added to the cart."/n)
 
 # Display items
-    elif action =="2"
+    elif action =="2":
        print(f"{display_items}The contents of the shopping cart are: ")
        for i in range (len(cart)):
        cart =[]
@@ -32,17 +32,17 @@ action = input("Please enter an action: 1,2,3,4,5 "/n)
        print(f"-{i+1},{item}, ${price}.2f") #Python starts counting at zero. Number of item, item, price.
 
 #Remove items
-    elif action =="3"
+    elif action =="3":
        remove_items = input("What item would you like to remove? ")
        items.pop(remove_items)
        print(f"{remove_items} has been removed.")
 
 # Show total
-    elif action =="4"
+    elif action =="4":
        for item in price:
        total = total + item  
        print(total)
 
 #quit
-     elif action =="5"
+     elif action =="5":
         print("Thank you. Goodbye.")
