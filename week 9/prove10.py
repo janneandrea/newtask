c162f134ac6c5f973a0e5ed27ff35ac1cd848e89
janneandrea <janneandrea@byui.edu>
2021-03-12 14:12:17 -0500

    intent commit

--- week 9/prove10.py
+++ week 9/prove10.py
@@ -29,7 +29,7 @@ while action !="5":
     elif action =="2":
         cart =[]
         prices = []
-        for i in range (len(add_items)):
+        for cart in range (len(add_items)):
             print(f"{i+1},{add_items}, ${price_items}") #Python starts counting at zero. Number of item, item, price.
 
 #Remove items
