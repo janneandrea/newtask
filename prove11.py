603dd8b81195e0d0a03a967160d1d25085387fcd
janneandrea <janneandrea@byui.edu>
2021-03-18 23:43:11 -0500

    intent commit

--- prove11.py
+++ prove11.py
@@ -1,11 +1,12 @@
-with open ("expectancy.txt") as life_expectancy : #opens and closes the text file
+with open ("expectancy.txt") as life_expectancy: #opens and closes the text file
      for line in life_expectancy:
-        line= line.strip() #bye blank space
-        parts = line.split(" ") # divides it into parts
+        parts = line.split(",") # divides it into parts
         
         entity = parts[0] #entity
         code = parts[1] #code
         year = parts[2] #year
         life_expec = parts[3] #year
         
-        print(f"{entity},{code},{year},{life_expec}")
+        print(f"{entity},{code},{year},{life_expec.strip()}") 
+
+        
