dae34daa81d68279ebfaab018cf0eb229ef2f84f
janneandrea <janneandrea@byui.edu>
2021-03-24 00:06:50 -0500

    intent commit

--- week11/prove11.py
+++ week11/prove11.py
@@ -8,5 +8,13 @@ with open ("life_expectancy.csv") as life_expectancy: #opens and closes the text
         life_expec = parts[3] #year
         
         print(f"{entity},{code},{year},{life_expec.strip()}") 
-
         
+user_choice = input("Enter the year of interest: ")
+
+print(f"The overall max life expectancy is: max{life_expec} from {entity} in {year}")
+print(f"The overall min life expectancy is: min{life_expec} from {entity} in {year}")
+
+print(f"For the year {user_choice}")
+print(f"The average life expectancy across all countries was {}")
+print(f"The max life expectancy was in {entity}with {life_expec}")
+print(f"The min life expectancy was in {entity} with {life_expec}")
