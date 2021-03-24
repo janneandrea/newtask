7461720d52f4d2a3be2675489470e95015c12fc5
janneandrea <janneandrea@byui.edu>
2021-03-24 17:31:45 -0500

    intent commit

--- week11/prove11.py
+++ week11/prove11.py
@@ -10,7 +10,7 @@ with open ("life_expectancy.csv") as life_expectancy: #opens and closes the text
         entity = parts[0] #entity
         code = parts[1] #code
         year = parts[2] #year
-        life_expec = parts[3] #year
+        life_expec = float(parts[3])#year
         
         print(f"{entity},{code},{year},{life_expec.strip()}") 
         
@@ -18,4 +18,13 @@ with open ("life_expectancy.csv") as life_expectancy: #opens and closes the text
             max_expect = life_expec
             print(f"The overall max life expectancy is: {max_expect} from {entity} in {year}")
     
-       
+        if life_expec < min_expect
+            min_expect = life_expec
+            print(f"The overall min life expectancy is: {min_expect} from {entity} in {year}")
+            print(f"For the year {user_choice}")
+            
+            avg= mean(life_expec)
+            print("The average life expectancy across all countries was", round(avg,2))
+            print(f"The max life expectancy was in {entity}with {life_expec}")
+            print(f"The min life expectancy was in {entity} with {life_expec}")
+
